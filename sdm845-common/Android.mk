c087984fcea41a7d99f167daaf550b6f6d33759e
Bruno Martins <bgcngm@gmail.com>
2019-09-26 00:13:55 +0100

    sdm845-common: Move offline charging daemon to new path
    The charger daemon has been moved to /system/bin.
    
    Change-Id: I19e5752667342e6ac3935cd124355b2725517748

--- sdm845-common/Android.mk
+++ sdm845-common/Android.mk
@@ -160,9 +160,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := chargeonlymode
 LOCAL_MODULE_OWNER := xiaomi
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
-LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
-LOCAL_SRC_FILES := proprietary/rootfs/sbin/chargeonlymode
+LOCAL_SRC_FILES := proprietary/bin/chargeonlymode
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 include $(BUILD_PREBUILT)
